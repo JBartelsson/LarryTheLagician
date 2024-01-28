ae18478267c938522aa908129cb777033f1f5241
JanaLuksch <jana_99@web.de>
2024-01-28 12:11:47 +0100

    Menu Controller

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+   public CanvasGroup OptionPanel;
+
+   public void PlayGame()
+   {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+   }
+  
+   public void Credits()
+   {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +2);
+   }
+
+   public void Quit()
+   {
+    Application.Quit();
+   }
+}
